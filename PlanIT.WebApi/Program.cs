1867e6ec1a7575fd5a6c8b3b64c58f6ae8cdc565
lucianoortiz97 <lucianodaniel_o@hotmail.com>
2025-12-16 21:30:14 -0300

    Mis fix antes que mati

--- PlanIT.WebApi/Program.cs
+++ PlanIT.WebApi/Program.cs
@@ -13,6 +13,7 @@ using PlanIT.Infrastructure.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.OpenApi.Models;
 
 // ========================================================================================================================
 // 0. Builder, aca inicia la aplicacion 
@@ -26,7 +27,37 @@ var builder = WebApplication.CreateBuilder(args);
 // ===========================================================================================================================
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setup =>
+{
+    // Configuración para que aparezca el botón "Authorize" con JWT
+    setup.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
